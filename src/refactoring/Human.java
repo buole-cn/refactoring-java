a62b2f7fed2ccc1a85503cf5eac79b10020daacb
tinchen <tin.chen@jii.tw>
2020-04-01 20:57:42 +0800

    第二個範例：Human - Extract a class

--- src/refactoring/Human.java
+++ src/refactoring/Human.java
@@ -3,21 +3,17 @@ package refactoring;
 public class Human {
 	private String name;
 	private int age;
-	private String country;
-	private String city;
-	private String street;
-	private String house;
-	private String quarter;
+	private Address address = new Address();
 
 	public Human(String name, int age, String country, String city, String street, String house, String quarter) {
 		super();
 		this.name = name;
 		this.age = age;
-		this.country = country;
-		this.city = city;
-		this.street = street;
-		this.house = house;
-		this.quarter = quarter;
+		this.address.country = country;
+		this.address.city = city;
+		this.address.street = street;
+		this.address.house = house;
+		this.address.quarter = quarter;
 	}
 
 	public String getName() {
@@ -29,8 +25,24 @@ public class Human {
 	}
 
 	public String getFullAddress() {
-		StringBuilder result = new StringBuilder();
-		return result.append(country).append(", ").append(city).append(", ").append(street).append(", ").append(house)
-				.append(" ").append(quarter).toString();
+		return this.address.getFullAddress();
 	}
+	
+	public static class Address {
+		public String country;
+		public String city;
+		public String street;
+		public String house;
+		public String quarter;
+
+		public Address() {
+		}
+
+		public String getFullAddress() {
+			StringBuilder result = new StringBuilder();
+			return result.append(country).append(", ").append(city).append(", ").append(street).append(", ").append(house)
+					.append(" ").append(quarter).toString();
+		}
+	}
+
 }
